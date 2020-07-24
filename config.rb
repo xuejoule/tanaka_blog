db1f611f4a48292367e53dba9c90ded751f226d2
Manabu Tanaka <tanaka@trust-software.co.jp>
2020-07-24 13:57:02 +0900

    写真添付テスト

--- config.rb
+++ config.rb
@@ -54,6 +54,7 @@ activate :blog do |blog|
   blog.sources = "{year}-{month}-{day}-{title}.html"
   blog.permalink = "{year}/{month}/{day}/{title}.html"
   blog.default_extension = ".md"
+  config.sources = "articles/{year}/{month}/{day}/{title}/index.html"
 end
 
 helpers do
